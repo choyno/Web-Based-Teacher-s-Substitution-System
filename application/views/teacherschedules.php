8fc7853d3e236e629e220e4f5b64d4487db7b5d5
Ariel Lopez <ariel.lopezf@gmail.com>
2012-08-06 18:42:29 -0700

    additional admin control

--- application/views/teacherschedules.php
+++ application/views/teacherschedules.php
@@ -21,15 +21,12 @@
 
 			</form>
 			<div class="widget widget-table">
-					
-						<div class="widget-header">
-							<span class="icon-list"></span>
-							<h3 class="icon chart">Data Table Plugin</h3>		
-						</div>
-					
-						<div class="widget-content">
-							
-							<table class="table table-bordered table-striped data-table">
+				<div class="widget-header">
+					<span class="icon-list"></span>
+					<h3 class="icon chart">Data Table Plugin</h3>		
+				</div>
+				<div class="widget-content">
+					<table class="table table-bordered table-striped data-table">
 						<thead>
 							<tr>
 								<th>Teacher</th>
@@ -40,48 +37,190 @@
 								<th>Schedule</th>
 							</tr>
 						</thead>
-						<tbody>
-							<tr>
-								<th>Kenneth B Barsalote</th>
-								<th>ICN 22</th>
-								<th>Room 302</th>
-								<th>IT704</th>
-								<th>1:00 -2:00</th>
-								<th>MWF</th>
-							</tr>
-	<tr>
-								<th>John  B Sable</th>
-								<th>ICN 23</th>
-								<th>Room 305</th>
-								<th>IT703</th>
-								<th>2:00 -3:00</th>
-								<th>TTH</th>
-							</tr>
-							<tr>
-								<th>Kenneth B Barsalote</th>
-								<th>ICN 22</th>
-								<th>Room 302</th>
-								<th>IT704</th>
-								<th>1:00 -2:00</th>
-								<th>MWF</th>
-							</tr>
-	<tr>
-								<th>John  B Sable</th>
-								<th>ICN 23</th>
-								<th>Room 305</th>
-								<th>IT703</th>
-								<th>2:00 -3:00</th>
-								<th>TTH</th>
-							</tr>
-																				
-						</tbody>
-					</table>
-						</div> <!-- .widget-content -->
-					
-					<div style="float:right;">
+				        <tbody>
+						<tr>
+							<th>Kenneth B Barsalote</th>
+							<th>ICN 22</th>
+							<th>Room 302</th>
+							<th>IT704</th>
+							<th>1:00 -2:00</th>
+							<th>MWF</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>Kenneth B Barsalote</th>
+							<th>ICN 22</th>
+							<th>Room 302</th>
+							<th>IT704</th>
+							<th>1:00 -2:00</th>
+							<th>MWF</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>Kenneth B Barsalote</th>
+							<th>ICN 22</th>
+							<th>Room 302</th>
+							<th>IT704</th>
+							<th>1:00 -2:00</th>
+							<th>MWF</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>Kenneth B Barsalote</th>
+							<th>ICN 22</th>
+							<th>Room 302</th>
+							<th>IT704</th>
+							<th>1:00 -2:00</th>
+							<th>MWF</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>Kenneth B Barsalote</th>
+							<th>ICN 22</th>
+							<th>Room 302</th>
+							<th>IT704</th>
+							<th>1:00 -2:00</th>
+							<th>MWF</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>Kenneth B Barsalote</th>
+							<th>ICN 22</th>
+							<th>Room 302</th>
+							<th>IT704</th>
+							<th>1:00 -2:00</th>
+							<th>MWF</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>Kenneth B Barsalote</th>
+							<th>ICN 22</th>
+							<th>Room 302</th>
+							<th>IT704</th>
+							<th>1:00 -2:00</th>
+							<th>MWF</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>
+						<tr>
+							<th>Kenneth B Barsalote</th>
+							<th>ICN 22</th>
+							<th>Room 302</th>
+							<th>IT704</th>
+							<th>1:00 -2:00</th>
+							<th>MWF</th>
+						</tr>
+						<tr>
+							<th>John  B Sable</th>
+							<th>ICN 23</th>
+							<th>Room 305</th>
+							<th>IT703</th>
+							<th>2:00 -3:00</th>
+							<th>TTH</th>
+						</tr>										
+				        </tbody>
+			         </table>
+				</div> <!-- .widget-content -->
+				<div style="float:right;">
 					<button class="btn btn-large btn-blue">Save</button>
 				</div>
-				</div> <!-- .widget -->
+		   </div> <!-- .widget -->
 
 		</div>
 
