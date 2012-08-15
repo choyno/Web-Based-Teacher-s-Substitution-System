63bc4beb42ff29389ba624dbc1f3f8f352edd26d
ariel.lopezf@gmail.com <ariel.lopezf@gmail.com>
2012-08-15 05:33:46 -0700

    delete

--- application/views/base/template.php
+++
@@ -1,5 +0,0 @@
-<?php $this -> load -> view ('base/header');?>
-<?php $this -> load -> view ('base/navigation');?>
-<?php $this -> load -> view ($mainContent);?>	
-<?php $this -> load -> view ('base/headernav');?>
-<?php $this -> load -> view ('base/footer');?>
