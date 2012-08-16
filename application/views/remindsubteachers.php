f89b2a67aa53ed53fdf9541a160af35bcfbb42ed
ariel.lopezf@gmail.com <ariel.lopezf@gmail.com>
2012-08-15 19:15:39 -0700

    choyno changes

--- application/views/remindsubteachers.php
+++ application/views/remindsubteachers.php
@@ -1,71 +1,106 @@
-<div id="content">		
+<div id="content">			
 	<div id="contentHeader">
-		<h1>Reminded Substitute </h1>
+		<h1>Remind Substitute Teachers</h1>
 	</div> <!-- #contentHeader -->	
 	<div class="container">
 		<div class="grid-24">
-			
 			<div class="widget widget-table">
-					
-						<div class="widget-header">
-							<span class="icon-list"></span>
-							<h3 class="icon chart">Data Table Plugin</h3>		
-						</div>
-					
-						<div class="widget-content">
-							
-							<table class="table table-bordered table-striped data-table">
+				<div class="widget-header">
+					<span class="icon-list"></span>
+					<h3 class="icon chart">Substitute Information</h3>		
+				</div>
+				<div class="widget-content">
+					<table class="table table-bordered table-striped data-table">
 						<thead>
 							<tr>
-								<th>Teacher</th>
-								<th>Subject Code</th>
+								<th>Substitute Teacher</th>
+								<th>Absent Teacher</th>
+								<th>Subject</th>
 								<th>Room #</th>
-								<th>Section</th>
-								<th>Time</th>
-								<th>Schedule</th>
+								<th>Request Date</th>
+								<th>Start Time</th>
+								<th>End Time</th>
+								<th>Options</th>
 							</tr>
 						</thead>
-						<tbody>
+				        <tbody>
 							<tr>
-								<th>Kenneth B Barsalote</th>
-								<th>ICN 22</th>
-								<th>Room 302</th>
-								<th>IT704</th>
-								<th>1:00 -2:00</th>
-								<th>MWF</th>
-							</tr>
-	<tr>
-								<th>John  B Sable</th>
-								<th>ICN 23</th>
+								<th>Roxane B Durano Roxane B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>It 704 </th>
 								<th>Room 305</th>
-								<th>IT703</th>
-								<th>2:00 -3:00</th>
-								<th>TTH</th>
+								<th>8/14/12</th>
+								<th>1:00</th>
+								<th>2:00</th>
+								<th>
+									<a href="#">Remind</a>
+								</th>
 							</tr>
 							<tr>
-								<th>Kenneth B Barsalote</th>
-								<th>ICN 22</th>
-								<th>Room 302</th>
-								<th>IT704</th>
-								<th>1:00 -2:00</th>
-								<th>MWF</th>
-							</tr>
-	<tr>
-								<th>John  B Sable</th>
-								<th>ICN 23</th>
+								<th>Roxane B Durano Roxane B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>It 704 </th>
 								<th>Room 305</th>
-								<th>IT703</th>
-								<th>2:00 -3:00</th>
-								<th>TTH</th>
+								<th>8/14/12</th>
+								<th>1:00</th>
+								<th>2:00</th>
+								<th>
+									<a href="#">Remind</a>
+								</th>
 							</tr>
-																				
-						</tbody>
-					</table>
-						</div> <!-- .widget-content -->
-					
-				</div> <!-- .widget -->
-		</div>
+
+							<tr>
+								<th>Roxane B Durano Roxane B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>It 704 </th>
+								<th>Room 305</th>
+								<th>8/14/12</th>
+								<th>1:00</th>
+								<th>2:00</th>
+								<th>
+									<a href="#">Remind</a>
+								</th>
+							</tr>
+
+							<tr>
+								<th>Roxane B Durano Roxane B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>It 704 </th>
+								<th>Room 305</th>
+								<th>8/14/12</th>
+								<th>1:00</th>
+								<th>2:00</th>
+								<th>
+									<a href="#">Remind</a>
+								</th>
+							</tr>
+
+<tr>
+								<th>Roxane B Durano Roxane B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>It 704 </th>
+								<th>Room 305</th>
+								<th>8/14/12</th>
+								<th>1:00</th>
+								<th>2:00</th>
+								<th>
+									<a href="#">Remind</a>
+								</th>
+							</tr>
+
+
+
+						
+
+
+				        </tbody>
+			         </table>
+				</div> <!-- .widget-content -->
+			   </div> <!-- .widget -->
+			</div> <!-- .widget-content -->
+		</div> <!-- .widget -->	
 	</div> <!-- .grid -->			
 </div>
-
-
+			
+			
+			
--- application/views/vouchteachers.php
+++ application/views/vouchteachers.php
@@ -1,71 +1,117 @@
-<div id="content">		
+<div id="content">			
 	<div id="contentHeader">
-		<h1>Vouch Teacher's</h1>
+		<h1>Vouch Teachers</h1>
 	</div> <!-- #contentHeader -->	
 	<div class="container">
 		<div class="grid-24">
-			
 			<div class="widget widget-table">
-					
-						<div class="widget-header">
-							<span class="icon-list"></span>
-							<h3 class="icon chart">Data Table Plugin</h3>		
-						</div>
-					
-						<div class="widget-content">
-							
-							<table class="table table-bordered table-striped data-table">
+				<div class="widget-header">
+					<span class="icon-list"></span>
+					<h3 class="icon chart">Vouch Information</h3>		
+				</div>
+				<div class="widget-content">
+					<table class="table table-bordered table-striped data-table">
 						<thead>
 							<tr>
-								<th>Teacher</th>
-								<th>Subject Code</th>
+								<th>Substitute Teacher</th>
+								<th>Absent Teacher</th>
+								<th>Subject</th>
 								<th>Room #</th>
-								<th>Section</th>
-								<th>Time</th>
-								<th>Schedule</th>
+								<th>Request Date</th>
+								<th>Start Time</th>
+								<th>End Time</th>
+								<th>Options</th>
 							</tr>
 						</thead>
-						<tbody>
+				        <tbody>
 							<tr>
-								<th>Kenneth B Barsalote</th>
-								<th>ICN 22</th>
-								<th>Room 302</th>
-								<th>IT704</th>
-								<th>1:00 -2:00</th>
-								<th>MWF</th>
-							</tr>
-	<tr>
-								<th>John  B Sable</th>
-								<th>ICN 23</th>
+								<th>Roxane B Durano Roxane B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>It 704 </th>
 								<th>Room 305</th>
-								<th>IT703</th>
-								<th>2:00 -3:00</th>
-								<th>TTH</th>
-							</tr>
+								<th>8/14/12</th>
+								<th>1:00</th>
+								<th>2:00</th>
+								<th>
+									<a href="#">Edit</a>|
+									<a href="#">Done</a>
+								</th>
+							</tr>	
 							<tr>
-								<th>Kenneth B Barsalote</th>
-								<th>ICN 22</th>
-								<th>Room 302</th>
-								<th>IT704</th>
-								<th>1:00 -2:00</th>
-								<th>MWF</th>
-							</tr>
-	<tr>
-								<th>John  B Sable</th>
-								<th>ICN 23</th>
+								<th>John B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>ICN23 </th>
 								<th>Room 305</th>
-								<th>IT703</th>
-								<th>2:00 -3:00</th>
-								<th>TTH</th>
-							</tr>
-																				
-						</tbody>
-					</table>
-						</div> <!-- .widget-content -->
-					
-				</div> <!-- .widget -->
-		</div>
+								<th>8/15/12</th>
+								<th>3:00</th>
+								<th>4:00</th>
+								<th>
+									<a href="#">Edit</a>|
+									<a href="#">Done</a>
+								</th>
+							</tr>	
+							<tr>
+								<th>Roxane B Durano Roxane B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>It 704 </th>
+								<th>Room 305</th>
+								<th>8/14/12</th>
+								<th>1:00</th>
+								<th>2:00</th>
+								<th>
+									<a href="#">Edit</a>|
+									<a href="#">Done</a>
+								</th>
+							</tr>	
+							<tr>
+								<th>John B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>ICN23 </th>
+								<th>Room 305</th>
+								<th>8/15/12</th>
+								<th>3:00</th>
+								<th>4:00</th>
+								<th>
+									<a href="#">Edit</a>|
+									<a href="#">Done</a>
+								</th>
+							</tr>	
+<tr>
+								<th>John B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>ICN23 </th>
+								<th>Room 305</th>
+								<th>8/15/12</th>
+								<th>3:00</th>
+								<th>4:00</th>
+								<th>
+									<a href="#">Edit</a>|
+									<a href="#">Done</a>
+								</th>
+							</tr>	
+							<tr>
+								<th>John B Durano</th>
+								<th>Ariel Lopez F.</th>
+								<th>ICN23 </th>
+								<th>Room 305</th>
+								<th>8/15/12</th>
+								<th>3:00</th>
+								<th>4:00</th>
+								<th>
+									<a href="#">Edit</a>|
+									<a href="#">Done</a>
+								</th>
+							</tr>	
+
+
+				        </tbody>
+			         </table>
+				</div> <!-- .widget-content -->
+			   </div> <!-- .widget -->
+			</div> <!-- .widget-content -->
+		</div> <!-- .widget -->	
 	</div> <!-- .grid -->			
 </div>
-
-
+			
+			
+			
