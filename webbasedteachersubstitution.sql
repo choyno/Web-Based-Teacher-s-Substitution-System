1ee0c5eccf35faf803737d336eb02b268843f974
choyno <ariel.lopezf@gmail.com>
2012-08-27 17:25:33 -0700

    SQL

--- webbasedteachersubstitution.sql
+++ webbasedteachersubstitution.sql
@@ -1,20 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1
+-- version 2.11.0
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 20, 2012 at 07:47 PM
--- Server version: 5.5.20
--- PHP Version: 5.3.10
+-- Generation Time: Aug 27, 2012 at 05:24 PM
+-- Server version: 5.0.45
+-- PHP Version: 5.2.4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `webbasedteachersubstitution`
@@ -23,221 +16,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ci_sessions`
+-- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `ci_sessions` (
-  `session_id` varchar(40) NOT NULL DEFAULT '0',
-  `ip_address` varchar(45) NOT NULL DEFAULT '0',
-  `user_agent` varchar(120) NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text NOT NULL,
-  PRIMARY KEY (`session_id`),
-  KEY `last_activity_idx` (`last_activity`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `day_codes`
---
-
-CREATE TABLE IF NOT EXISTS `day_codes` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `departments`
---
-
-CREATE TABLE IF NOT EXISTS `departments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `first_name` varchar(30) NOT NULL,
+  `last-name` varchar(30) NOT NULL,
+  `email_addess` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  `user_type` varchar(10) NOT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `departments`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `departments` (`id`, `name`) VALUES
-(1, 'Css');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reasons`
---
-
-CREATE TABLE IF NOT EXISTS `reasons` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `status`
---
-
-CREATE TABLE IF NOT EXISTS `status` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `subjects`
---
-
-CREATE TABLE IF NOT EXISTS `subjects` (
-  `id` int(11) NOT NULL,
-  `code` varchar(30) DEFAULT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `substitutes`
---
-
-CREATE TABLE IF NOT EXISTS `substitutes` (
-  `id` int(11) NOT NULL,
-  `subtitute_teacher_id` int(11) NOT NULL,
-  `reasons_id` int(11) NOT NULL,
-  `teachers_subject_id` int(11) NOT NULL,
-  `planned` tinyint(1) DEFAULT NULL,
-  `requested_at` datetime DEFAULT NULL,
-  `notes` text,
-  `assigned_by_user_id` int(11) NOT NULL,
-  `approved_at` datetime DEFAULT NULL,
-  `approved_by_userd_id` int(11) DEFAULT NULL,
-  `status_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`subtitute_teacher_id`,`reasons_id`,`teachers_subject_id`,`assigned_by_user_id`,`status_id`),
-  KEY `subtitute_teacher_id` (`subtitute_teacher_id`),
-  KEY `reasons_id` (`reasons_id`),
-  KEY `teachers_subject_id` (`teachers_subject_id`),
-  KEY `assigned_by_user_id` (`assigned_by_user_id`),
-  KEY `status_id` (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teachers`
---
-
-CREATE TABLE IF NOT EXISTS `teachers` (
-  `id` int(11) NOT NULL,
-  `firstname` varchar(30) DEFAULT NULL,
-  `lastname` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teachers_subjects`
---
-
-CREATE TABLE IF NOT EXISTS `teachers_subjects` (
-  `id` int(11) NOT NULL,
-  `teacher_id` int(11) NOT NULL,
-  `subject_id` int(11) NOT NULL,
-  `time_start` time DEFAULT NULL,
-  `time_end` time DEFAULT NULL,
-  `room_num` varchar(45) DEFAULT NULL,
-  `day_code_id` int(11) NOT NULL,
-  `section` varchar(45) DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`,`teacher_id`,`subject_id`,`day_code_id`),
-  KEY `teacher_id` (`teacher_id`),
-  KEY `subject_id` (`subject_id`),
-  KEY `day_code_id` (`day_code_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fullname` varchar(30) DEFAULT NULL,
-  `username` varchar(30) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL,
-  `user_type_id` int(11) NOT NULL,
-  `department_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`user_type_id`,`department_id`),
-  KEY `user_type_id` (`user_type_id`),
-  KEY `department_id` (`department_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `fullname`, `username`, `password`, `user_type_id`, `department_id`) VALUES
-(1, 'Ariel Lopez', 'admin', '21232f297a57a5a743894a0e4a801f', 1, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_types`
---
-
-CREATE TABLE IF NOT EXISTS `user_types` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `user_types`
---
-
-INSERT INTO `user_types` (`id`, `name`) VALUES
-(1, 'Department Head');
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `substitutes`
---
-ALTER TABLE `substitutes`
-  ADD CONSTRAINT `substitutes_ibfk_1` FOREIGN KEY (`subtitute_teacher_id`) REFERENCES `teachers` (`id`),
-  ADD CONSTRAINT `substitutes_ibfk_2` FOREIGN KEY (`reasons_id`) REFERENCES `reasons` (`id`),
-  ADD CONSTRAINT `substitutes_ibfk_3` FOREIGN KEY (`teachers_subject_id`) REFERENCES `teachers_subjects` (`id`),
-  ADD CONSTRAINT `substitutes_ibfk_4` FOREIGN KEY (`assigned_by_user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `substitutes_ibfk_5` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`);
-
---
--- Constraints for table `teachers_subjects`
---
-ALTER TABLE `teachers_subjects`
-  ADD CONSTRAINT `teachers_subjects_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`),
-  ADD CONSTRAINT `teachers_subjects_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`),
-  ADD CONSTRAINT `teachers_subjects_ibfk_3` FOREIGN KEY (`day_code_id`) REFERENCES `day_codes` (`id`);
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_type_id`) REFERENCES `user_types` (`id`),
-  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `user` (`id`, `first_name`, `last-name`, `email_addess`, `username`, `password`, `department`, `user_type`) VALUES
+(1, 'Ariel', 'Lopez', 'lopez_a3a@yahoo.com', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'CCS', 'admin');
