460928ed0346ed1ff4534d65e0d544f828f9a644
Choyno <ariel.lopezf@gmail.com>
2012-09-01 19:53:25 -0700

    database

--- webbasedteachersubstitution.sql
+++ webbasedteachersubstitution.sql
@@ -1,13 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.0
+-- version 3.4.10.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 27, 2012 at 05:24 PM
--- Server version: 5.0.45
--- PHP Version: 5.2.4
+-- Generation Time: Sep 02, 2012 at 02:52 AM
+-- Server version: 5.5.20
+-- PHP Version: 5.3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `webbasedteachersubstitution`
@@ -16,24 +23,103 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `departments`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL auto_increment,
-  `first_name` varchar(30) NOT NULL,
-  `last-name` varchar(30) NOT NULL,
-  `email_addess` varchar(30) NOT NULL,
-  `username` varchar(30) NOT NULL,
+CREATE TABLE IF NOT EXISTS `departments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `name`) VALUES
+(3, 'CCS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(50) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('938eb986c2f310ae16224a7a1f2ddb71', '127.0.0.1', 'Mozilla/5.0 (Windows NT 5.1; rv:14.0) Gecko/201001', 1346142703, 'a:1:{s:2:"id";s:2:"25";}'),
+('b9549ad6b298726cf6491df9ca1917c8', '127.0.0.1', 'Mozilla/5.0 (Windows NT 5.1; rv:14.0) Gecko/201001', 1346153469, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(20) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `department` varchar(20) NOT NULL,
-  `user_type` varchar(10) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `user_types_id` int(11) NOT NULL,
+  `departments_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_types_id` (`user_types_id`),
+  KEY `departments_id` (`departments_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `user` (`id`, `first_name`, `last-name`, `email_addess`, `username`, `password`, `department`, `user_type`) VALUES
-(1, 'Ariel', 'Lopez', 'lopez_a3a@yahoo.com', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'CCS', 'admin');
+INSERT INTO `users` (`id`, `fullname`, `status`, `username`, `password`, `user_types_id`, `departments_id`) VALUES
+(3, 'Ariel Lopez', 1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_types`
+--
+
+CREATE TABLE IF NOT EXISTS `user_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `user_types`
+--
+
+INSERT INTO `user_types` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'Department Head'),
+(3, 'Checker');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_types_id`) REFERENCES `user_types` (`id`),
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`departments_id`) REFERENCES `departments` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
